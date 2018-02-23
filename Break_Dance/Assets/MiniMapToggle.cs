d54a7a5abaa5cd3bc5305c59d7df4a3f8b471d7b
BeeBeeCue <kristoferknutsen@hotmail.com>
2018-02-23 15:52:43 +0200

    Minimap update

---
+++ Break_Dance/Assets/MiniMapToggle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MiniMapToggle : MonoBehaviour 
+{
+
+	public bool mapActive = false;
+	public GameObject MiniMapObject;
+
+
+	
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			
+
+			if (mapActive)
+			{
+				Debug.Log("You pressed M");
+				MiniMapObject.SetActive(true);
+				mapActive = true;
+				Debug.Log("Map is on");
+				return;
+			}
+
+			else
+			{
+				Debug.Log("You pressed M");
+				MiniMapObject.SetActive(false);
+				mapActive = false;
+				Debug.Log("Map is off");
+				return;
+			}
+			
+		}
+
+		
+
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
